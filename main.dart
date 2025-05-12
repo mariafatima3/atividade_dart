c39d061412df07d202d61a62919ac9b518b783c1
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-05-12 17:38:22 +0000

    12/05/25

--- main.dart
+++ main.dart
@@ -1,3 +1,18 @@
 void main() {
   print('Olá, Dart!');
+
+  int idade = 28;
+  print("Idade: $idade");
+
+  double altura = 1.75;
+  print("Altura: $altura");
+ 
+  var animal = "Frajola";
+  print("Animal: $animal");
+
+ 
+
+
+  
+
 }
