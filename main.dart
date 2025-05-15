de3d1b21baefe739ec334c65cd2336977a77dcaa
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-05-15 18:14:10 +0000

    15/05/25

--- main.dart
+++ main.dart
@@ -1,14 +1,44 @@
 void main() {
   print('Olá, Dart!');
+// 1. Declare uma variável inteira para armazenar a idade de uma pessoa e atribua o valor 28.
 
   int idade = 28;
   print("Idade: $idade");
 
+// 2. Crie uma variável double para guardar a altura de uma pessoa em metros e atribua o valor 1.75.
+
   double altura = 1.75;
   print("Altura: $altura");
- 
+
+ // 3. Defina uma variável String para armazenar o nome de um animal de estimação e atribua um nome de sua escolha.
+
   var animal = "Frajola";
   print("Animal: $animal");
+
+// Crie uma variável bool para indicar se uma lâmpada está ligada e atribua o valor false.
+
+  bool lambada_ligada = false;
+  print("Lambada_ligada: $lambada_ligada");
+
+// 5. Declare duas variáveis inteiras, a e b, com valores 10 e 3, respectivamente, e calcule a soma.
+
+  int a = 10;
+  int b = 3;
+  int soma = a + b;
+  print("Soma: $soma");
+
+// 6. Usando as variáveis do exercício anterior, calcule o resto da divisão de a por b.
+
+// 7. Crie uma variável double para o valor de uma compra e aplique um desconto de 10% usando operadores aritméticos.
+
+ 
+
+
+
+
+
+  
+
 
  
 
